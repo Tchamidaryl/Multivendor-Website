e15aabd4f9ee50a662a4fb31aec8523e38ea2000
Tchamidaryl <tchamidaryl@gmail.com>
2024-11-04 13:24:34 +0100

    Applying makePostRequest and rectifying submit button

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -4,6 +4,7 @@ import ImageInput from '@/components/FormInputs/ImageInput'
 import SubmitButton from '@/components/FormInputs/SubmitButton'
 import TextAreaInput from '@/components/FormInputs/TextAreaInput'
 import TextInput from '@/components/FormInputs/TextInput'
+import { makePostRequest } from '@/lib/apiRequest'
 import { generateSlug } from '@/lib/generateSlug'
 import React, { useState} from 'react'
 import { useForm } from 'react-hook-form'
@@ -11,7 +12,7 @@ import { useForm } from 'react-hook-form'
 export default function NewCategory() {
   const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(false)
-  const {register, handleSubmit, formState:{errors}} = useForm();
+  const {register, reset, handleSubmit, formState:{errors}} = useForm();
 
   async function onSubmit(data){
     {
@@ -27,6 +28,8 @@ export default function NewCategory() {
     data.slug = slug;
     data.imageUrl = imageUrl;
     console.log(data);
+    makePostRequest(setLoading, "api/categories", data, "Category", reset);
+    setImageUrl("")
   }
 
   return (
