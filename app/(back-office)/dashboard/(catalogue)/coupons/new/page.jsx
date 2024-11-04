3a65db6a618460825d896c5874f575401db86cbd
Tchamidaryl <tchamidaryl@gmail.com>
2024-11-04 13:58:47 +0100

    Working on coupons

---
+++ app/(back-office)/dashboard/(catalogue)/coupons/new/page.jsx
@@ -0,0 +1,64 @@
+'use client'
+import FormHeader from '@/components/backoffice/FormHeader'
+import SubmitButton from '@/components/FormInputs/SubmitButton'
+import TextInput from '@/components/FormInputs/TextInput'
+import { makePostRequest } from '@/lib/apiRequest'
+import { generateSlug } from '@/lib/generateSlug'
+import React, { useState} from 'react'
+import { useForm } from 'react-hook-form'
+
+export default function NewCoupon() {
+  const [loading, setLoading] = useState(false)
+  const {register, reset, handleSubmit, formState:{errors}} = useForm();
+
+  async function onSubmit(data){
+    {
+      /*
+      -id => auto()
+      -title
+      -code => auto()
+      -expiryDate
+      */
+    }
+    // const slug = generateSlug(data.title)
+    // data.slug = slug;
+    console.log(data);
+    // makePostRequest(setLoading, "api/categories", data, "Category", reset);
+    // setImageUrl("")
+  }
+
+  return (
+    <div>
+        <FormHeader title="New Coupon"/>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <TextInput
+              label="Coupon Title"
+              name="title"
+              register={register}
+              errors={errors}
+            />
+            <TextInput
+              label="Coupon Code"
+              name="couponCode"
+              register={register}
+              errors={errors}
+              className='w-full'
+            />
+            <TextInput
+              label="Coupon Expiry Date"
+              name="expiryDate"
+              type='date'
+              register={register}
+              errors={errors}
+              className='w-full'
+            />
+          </div>
+            <SubmitButton
+              isLoading={loading}
+              buttonTitle="Create Coupon" loadingButtonTitle="Creating Coupon please wait..."/>
+        </form>
+    </div>
+  )
+}
