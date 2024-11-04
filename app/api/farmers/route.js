d06c01bc03c0c1548772b1b853a2aebb7dff3967
Tchamidaryl <tchamidaryl@gmail.com>
2024-11-04 16:49:12 +0100

    FarmerUniqueCode rectified

--- app/api/farmers/route.js
+++ app/api/farmers/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 
 export async function POST(request){
     try{
-        const { code, contactPerson, contactPersonPhone, email, name, notes, phone, physicalAddress, terms} = await request.json();
-        const newFarmer = {code, contactPerson, contactPersonPhone, email, name, notes, phone, physicalAddress, terms};
+        const { farmerUniqueCode, contactPerson, contactPersonPhone, email, name, notes, phone, physicalAddress, terms} = await request.json();
+        const newFarmer = {farmerUniqueCode, contactPerson, contactPersonPhone, email, name, notes, phone, physicalAddress, terms};
         console.log(newFarmer);
         return NextResponse.json(newFarmer)
     }catch(error){
