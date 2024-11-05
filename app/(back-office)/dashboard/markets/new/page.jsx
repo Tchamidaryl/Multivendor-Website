75f19c4d9caf93a14618d04db52028a21a99ca9c
Tchamidaryl <tchamidaryl@gmail.com>
2024-11-05 14:39:37 +0100

    Market and route created

---
+++ app/(back-office)/dashboard/markets/new/page.jsx
@@ -0,0 +1,65 @@
+'use client'
+import FormHeader from '@/components/backoffice/FormHeader'
+import ImageInput from '@/components/FormInputs/ImageInput'
+import SubmitButton from '@/components/FormInputs/SubmitButton'
+import TextAreaInput from '@/components/FormInputs/TextAreaInput'
+import TextInput from '@/components/FormInputs/TextInput'
+import { makePostRequest } from '@/lib/apiRequest'
+import { generateSlug } from '@/lib/generateSlug'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+export default function NewBanner() {
+  const [logoUrl, setLogoUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const {register, reset, handleSubmit, formState:{errors}} = useForm();
+
+  async function onSubmit(data){
+    {
+      /*
+      -id => auto()
+      -title
+      -slug => auto()
+      -logo
+      -description
+      */
+    }
+    const slug = generateSlug(data.title);
+    data.slug = slug;
+    data.logoUrl = logoUrl;
+    console.log(data);
+    makePostRequest(setLoading, "api/markets", data, "Market", reset);
+    setLogoUrl("");
+  }
+
+  return (
+    <div>
+        <FormHeader title="New Market"/>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <TextInput
+              label="Market Title"
+              name="title"
+              register={register}
+              errors={errors}
+            />
+            {/* Configure this endpoint in the core.js */}
+            <ImageInput
+              imageUrl={logoUrl}
+              setImageUrl={setLogoUrl}
+              endpoint='marketLogoUploader'
+              label="Market Logo"
+            />
+            <TextAreaInput
+              label="Market Description"
+              name="description"
+              register={register}
+              errors={errors}
+            />
+          </div>
+            <SubmitButton isLoading={false} buttonTitle="Create Market" loadingButtonTitle="Creating Market please wait..."/>
+        </form>
+    </div>
+  )
+}
--- app/(back-office)/dashboard/markets/page.jsx
+++ app/(back-office)/dashboard/markets/page.jsx
@@ -1,9 +1,20 @@
+import PageHeader from '@/components/backoffice/PageHeader'
+import TableActions from '@/components/backoffice/TableActions'
 import React from 'react'
 
 export default function page() {
   return (
-    <div className="text-slate-900 dark:text-slate-200">
-        <h2>Markets</h2>
+    <div>
+      {/* Header */}
+      <PageHeader heading="Markets" href="/dashboard/markets/new" linkTitle="Add Market"/>
+
+      {/* Table Actions */}
+      {/* Export // Search // Bulk Delete */}
+      <TableActions/>
+
+        <div className="py-8 text-slate-900 dark:text-slate-200">
+          <h2>Table</h2>
+        </div>
     </div>
   )
 }
