08910bb40af77b59fcc32c1ce177b50db58dc760
Tchamidaryl <tchamidaryl@gmail.com>
2024-11-14 20:58:23 +0100

    Training completed and route created

--- app/(back-office)/dashboard/community/page.jsx
+++ app/(back-office)/dashboard/community/page.jsx
@@ -1,9 +1,20 @@
+import PageHeader from '@/components/backoffice/PageHeader'
+import TableActions from '@/components/backoffice/TableActions'
 import React from 'react'
 
 export default function page() {
   return (
-    <div className="text-slate-900 dark:text-slate-200">
-        <h2>Limi Community</h2>
+    <div>
+      {/* Header */}
+      <PageHeader heading="Limi Community Training" href="/dashboard/community/new" linkTitle="Add Training"/>
+
+      {/* Table Actions */}
+      {/* Export // Search // Bulk Delete */}
+      <TableActions/>
+
+        <div className="py-8 text-slate-900 dark:text-slate-200">
+          <h2>Table</h2>
+        </div>
     </div>
   )
 }
