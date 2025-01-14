d5955871b31e0aa92084cf127abb8b039cf457cc
Tchamidaryl <tchamidaryl@gmail.com>
2025-01-14 14:25:03 +0100

    product db and linking completed

--- app/(back-office)/dashboard/(catalogue)/products/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/products/new/page.jsx
@@ -1,172 +1,27 @@
-'use client'
-import FormHeader from '@/components/backoffice/FormHeader'
-import ImageInput from '@/components/FormInputs/ImageInput'
-import SelectInput from '@/components/FormInputs/SelectInput'
-import SubmitButton from '@/components/FormInputs/SubmitButton'
-import ArrayItemInput from '@/components/FormInputs/ArrayItemInput'
-import TextAreaInput from '@/components/FormInputs/TextAreaInput'
-import TextInput from '@/components/FormInputs/TextInput'
-import { makePostRequest } from '@/lib/apiRequest'
-import { generateSlug } from '@/lib/generateSlug'
-import React, { useState} from 'react'
-import { useForm } from 'react-hook-form'
-import ToggleInput from '@/components/FormInputs/ToggleInput'
+import NewProductForm from '@/components/backoffice/NewProductForm'
+import { getData } from '@/lib/getData'
+import React from 'react'
 
-export default function NewCategory() {
-  const [imageUrl, setImageUrl] = useState("");
-  const categories = [
-    {
-      id: 1,
-      title: "Category 1"
-    },
-    {
-      id: 2,
-      title: "Category 2"
-    },
-    {
-      id: 3,
-      title: "Category 3"
-    },
-  ];
-  const farmers = [
-    {
-      id: 1,
-      title: "Farmer 1"
-    },
-    {
-      id: 2,
-      title: "Farmer 2"
-    },
-    {
-      id: 3,
-      title: "Farmer 3"
-    },
-  ];
-  // TAGS
-  const [tags, setTags] = useState([]);
-  const [loading, setLoading] = useState(false)
-  const {register, reset, watch, handleSubmit, formState:{errors}} = useForm({
-    defaultValues:{
-      isActive: true,
-    }
-  });
-  const isActive = watch("isActive");
-  console.log(isActive);
+export default async function NewProduct() {
+  // Categories and farmers
+  const categoriesData = await getData("categories");
+  const categories = categoriesData.map((category) => {
+    return {
+      id: category.id,
+      title: category.title
+    };
+  })
 
-  async function onSubmit(data){
-    {
-      /*
-      -id => auto()
-      -title
-      -slug => auto()
-      -description
-      -image/images[]
-      -sku
-      -barcode
-      -productPrice
-      -salePrice
-      -category
-      -Farmer
-      -tags[]
-      */
+  const usersData = await getData("users");
+  const farmersData = usersData.filter((user) => user.role === "FARMER")
+  const farmers = farmersData.map((farmer) => {
+    return {
+      id: farmer.id,
+      title: farmer.name,
     }
-    const slug = generateSlug(data.title)
-    data.slug = slug;
-    data.imageUrl = imageUrl;
-    data.tags = tags;
-    console.log(data);
-    makePostRequest(setLoading, "api/products", data, "Product", reset);
-    setImageUrl("")
-  }
+  })
 
   return (
-    <div>
-        <FormHeader title="New Product"/>
-
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3">
-          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-            <TextInput
-              label="Product Title"
-              name="title"
-              register={register}
-              errors={errors}
-            />
-            <TextInput
-              label="Product SKU"
-              name="sku"
-              register={register}
-              errors={errors}
-              className='w-full'
-            />
-            <TextInput
-              label="Product Barcode"
-              name="barcode"
-              register={register}
-              errors={errors}
-              className='w-full'
-            />
-            <TextInput
-              label="Product Price(Before Discount)"
-              name="productPrice"
-              type='number'
-              register={register}
-              errors={errors}
-              className='w-full'
-            />
-            <TextInput
-              label="Product Sale Price(Discounted)"
-              name="salePrice"
-              type='number'
-              register={register}
-              errors={errors}
-              className='w-full'
-            />
-            <SelectInput
-              label="Select Category"
-              name="categoryId"
-              register={register}
-              errors={errors}
-              options={categories}
-              className='w-full'
-            />
-            <SelectInput
-              label="Select Farmer"
-              name="farmerId"
-              register={register}
-              errors={errors}
-              options={farmers}
-              className='w-full'
-            />
-            <ImageInput
-              imageUrl={imageUrl}
-              setImageUrl={setImageUrl}
-              endpoint='productImageUploader'
-              label="Product Image"
-            />
-
-            {/* TAGS */}
-            <ArrayItemInput setItems={setTags} items={tags} itemTitle="Tag"/>
-
-            <TextAreaInput
-              label="Product Description"
-              name="description"
-              register={register}
-              errors={errors}
-            />
-
-            <ToggleInput
-              label="Publish your Product"
-              name="isActive"
-              trueTitle="Active"
-              falseTitle="Inactive"
-              register={register}
-            />
-
-          </div>
-            <SubmitButton
-              isLoading={loading}
-              buttonTitle="Create Product" loadingButtonTitle="Creating product please wait..."/>
-        </form>
-    </div>
+    <NewProductForm categories={categories} farmers={farmers} />
   )
 }
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,32 +1,118 @@
+import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(request){
     {
         /*
-        -id => auto()
-        -title
-        -slug => auto()
-        -description
-        -image/images[]
-        -sku
         -barcode
+        -categoryId
+        -description
+        -userId:farmerId
+        -imageUrl
+        -isActive
+        -isWholesale
+        -productCode
         -productPrice
         -salePrice
-        -category
-        -Farmer
-        -tags[]
+        -sku
+        -slug
+        -tags
+        -title
+        -unit
+        -wholesalePrice
+        -wholesaleQty
+        -productStock
+        -qty
         */
     }
     try{
-        const productData = await request.json();
-        console.log(productData);
-        return NextResponse.json(productData)
+        const {
+            barcode,
+            categoryId,
+            description,
+            farmerId,
+            imageUrl,
+            isActive,
+            isWholesale,
+            productCode,
+            productPrice,
+            salePrice,
+            sku,
+            slug,
+            tags,
+            title,
+            unit,
+            wholesalePrice,
+            wholesaleQty,
+            productStock,
+            qty,
+        } = await request.json();
+        // Check if the product already exists in the db
+        const existingProduct = await db.product.findUnique({
+            where: {
+                slug,
+            },
+        });
+        if (existingProduct) {
+            return NextResponse.json({
+                data: null,
+                message: "Product already exists",
+            }, { status: 409 });
+        }
+        const newProduct = await db.product.create({
+            data: {
+                barcode,
+                categoryId,
+                description,
+                userId: farmerId,
+                imageUrl,
+                isActive,
+                isWholesale,
+                productCode,
+                productPrice:parseFloat(productPrice),
+                salePrice:parseFloat(salePrice),
+                sku,
+                slug,
+                tags,
+                title,
+                unit,
+                wholesalePrice:parseFloat(wholesalePrice),
+                wholesaleQty:parseInt(wholesaleQty),
+                productStock:parseInt(productStock),
+                qty:parseInt(qty),
+            },
+        });
+
+        console.log(newProduct);
+        return NextResponse.json(newProduct)
     }catch(error){
-        console.log(error)
+        console.log(error);
         return NextResponse.json({
             message: "Failed to create Product",
             error,
         },{ status: 500}
     )
+    }
+}
+
+export async function GET(request) {
+    try {
+        const products = await db.product.findMany({
+            orderBy: {
+                createdAt: "desc",
+            },
+        });
+        return NextResponse.json(products);
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {
+                message: "Failed to get Product",
+                error,
+            },
+            {
+                status: 500,
+            }
+        );
     }
 }
