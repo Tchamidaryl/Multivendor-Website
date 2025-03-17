9bdd582691a270d02291150011d35103c6e4dc02
Tchamidaryl <tchamidaryl@gmail.com>
2025-03-17 17:12:45 +0100

    Edit profile started

--- components/backoffice/Navbar.jsx
+++ components/backoffice/Navbar.jsx
@@ -1,7 +1,16 @@
-"use client"
-import { AlignJustify, Bell, LayoutDashboard, LogOut, Settings, Sun, User, X } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import {
+  AlignJustify,
+  Bell,
+  LayoutDashboard,
+  LogOut,
+  Settings,
+  Sun,
+  User,
+  X,
+} from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 import {
   DropdownMenu,
@@ -10,36 +19,46 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import ThemeSwitcherBtn from '../ThemeSwitcherBtn'
-import Link from 'next/link'
-import UserAvatar from './UserAvatar'
-import { useSession } from 'next-auth/react'
+} from "@/components/ui/dropdown-menu";
+import ThemeSwitcherBtn from "../ThemeSwitcherBtn";
+import Link from "next/link";
+import UserAvatar from "./UserAvatar";
+import { useSession } from "next-auth/react";
 
 export default function Navbar({ setShowSidebar, showSidebar }) {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
   if (status === "loading") {
-    return <p className=''>Loading...</p>
+    return <p className="">Loading...</p>;
   }
   return (
     <div className="flex items-center justify-between bg-white dark:bg-slate-800 text-slate-50 h-20 px-8 py-8 fixed top-0 w-full md:left-64 right-0 z-50 sm:pr-[20rem]">
-      <Link href={"/dashboard"} className="sm:hidden">Logo</Link>
+      <Link href={"/dashboard"} className="sm:hidden">
+        Logo
+      </Link>
       {/* Icons */}
-      <button onClick={() => setShowSidebar(!showSidebar)} className="text-lime-700 dark:text-lime-500">
-        <AlignJustify className=''/>
+      <button
+        onClick={() => setShowSidebar(!showSidebar)}
+        className="text-lime-700 dark:text-lime-500"
+      >
+        <AlignJustify className="" />
       </button>
       {/* 3 Icons */}
       <div className="flex space-x-3">
         {/* theme */}
-        <ThemeSwitcherBtn/>
+        <ThemeSwitcherBtn />
 
         {/* Notification */}
         <DropdownMenu>
           <DropdownMenuTrigger>
-            <button type="button" className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg">
-              <Bell className="text-lime-700 dark:text-lime-500"/>
+            <button
+              type="button"
+              className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg"
+            >
+              <Bell className="text-lime-700 dark:text-lime-500" />
               <span className="sr-only">Notifications</span>
-              <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">20</div>
+              <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">
+                20
+              </div>
             </button>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="px-4 py-2 pr-8">
@@ -48,16 +67,24 @@ export default function Navbar({ setShowSidebar, showSidebar }) {
 
             <DropdownMenuItem>
               <div className="flex items-center space-x-2">
-                <Image src='/profile.png' alt="User profile" width={200} height={200} className="w-8 h-8 rounded-full"/>
+                <Image
+                  src="/profile.png"
+                  alt="User profile"
+                  width={200}
+                  height={200}
+                  className="w-8 h-8 rounded-full"
+                />
                 <div className="flex flex-col space-y-1">
                   <p>Yellow Sweet Corn Stock out,</p>
                   <div className="flex items-center space-x-2">
-                    <p className="px-3 py-0.5 bg-red-700 text-white rounded-full text-sm">Stock Out</p>
+                    <p className="px-3 py-0.5 bg-red-700 text-white rounded-full text-sm">
+                      Stock Out
+                    </p>
                     <p>Dec 12 2021 - 12:40PM</p>
                   </div>
                 </div>
                 <button>
-                  <X/>
+                  <X />
                 </button>
               </div>
             </DropdownMenuItem>
@@ -65,16 +92,24 @@ export default function Navbar({ setShowSidebar, showSidebar }) {
 
             <DropdownMenuItem>
               <div className="flex items-center space-x-2">
-                <Image src='/profile.png' alt="User profile" width={200} height={200} className="w-8 h-8 rounded-full"/>
+                <Image
+                  src="/profile.png"
+                  alt="User profile"
+                  width={200}
+                  height={200}
+                  className="w-8 h-8 rounded-full"
+                />
                 <div className="flex flex-col space-y-1">
                   <p>Yellow Sweet Corn Stock out,</p>
                   <div className="flex items-center space-x-2">
-                    <p className="px-3 py-0.5 bg-red-700 text-white rounded-full text-sm">Stock Out</p>
+                    <p className="px-3 py-0.5 bg-red-700 text-white rounded-full text-sm">
+                      Stock Out
+                    </p>
                     <p>Dec 12 2021 - 12:40PM</p>
                   </div>
                 </div>
                 <button>
-                  <X/>
+                  <X />
                 </button>
               </div>
             </DropdownMenuItem>
@@ -82,27 +117,34 @@ export default function Navbar({ setShowSidebar, showSidebar }) {
 
             <DropdownMenuItem>
               <div className="flex items-center space-x-2">
-                <Image src='/profile.png' alt="User profile" width={200} height={200} className="w-8 h-8 rounded-full"/>
+                <Image
+                  src="/profile.png"
+                  alt="User profile"
+                  width={200}
+                  height={200}
+                  className="w-8 h-8 rounded-full"
+                />
                 <div className="flex flex-col space-y-1">
                   <p>Yellow Sweet Corn Stock out,</p>
                   <div className="flex items-center space-x-2">
-                    <p className="px-3 py-0.5 bg-red-700 text-white rounded-full text-sm">Stock Out</p>
+                    <p className="px-3 py-0.5 bg-red-700 text-white rounded-full text-sm">
+                      Stock Out
+                    </p>
                     <p>Dec 12 2021 - 12:40PM</p>
                   </div>
                 </div>
                 <button>
-                  <X/>
+                  <X />
                 </button>
               </div>
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-
           </DropdownMenuContent>
         </DropdownMenu>
 
         {/* My Account */}
-        <UserAvatar user={session?.user}/>
+        {status === "authenticated" && <UserAvatar user={session?.user} />}
       </div>
     </div>
-  )
+  );
 }
--- components/backoffice/UserAvatar.jsx
+++ components/backoffice/UserAvatar.jsx
@@ -13,9 +13,11 @@ import { LayoutDashboard, LogOut, Settings } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { generateInitials } from "@/lib/generateInitials";
 
-export default function UserAvatar({ user }) {
+export default function UserAvatar({ user = {} }) {
   const { name, image } = user;
+  const initials = generateInitials(name);
   const router = useRouter();
   async function handleLogout() {
     await signOut();
@@ -34,8 +36,8 @@ export default function UserAvatar({ user }) {
               className="w-8 h-8 rounded-full"
             />
           ) : (
-            <div className="w-8 h-8 rounded-full bg-blue-600 flex justify-center items-center">
-              <p className="text-slate-50">TD</p>
+            <div className="w-10 h-10 rounded-full bg-slate-800 shadow-md border border-slate-600 p-4 flex justify-center items-center">
+              <p className="text-slate-50">{initials}</p>
             </div>
           )}
         </button>
@@ -50,10 +52,13 @@ export default function UserAvatar({ user }) {
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <button className="flex items-center space-x-2">
+          <Link
+            href="/dashboard/profile"
+            className="flex items-center space-x-2"
+          >
             <Settings className="mr-2 h-4 w-4" />
             <span>Edit Profile</span>
-          </button>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
           <button
