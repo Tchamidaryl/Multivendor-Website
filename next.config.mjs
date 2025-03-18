129613eb09960f6d9aa7c8d627f11a2c4ada7ec4
Tchamidaryl <tchamidaryl@gmail.com>
2025-03-18 23:29:05 +0100

    test-6

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
+import path from "path"; // Add this import at the top
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'utfs.io',
-            },
+        {
+            protocol: "https",
+            hostname: "utfs.io",
+        },
         ],
+    },
+    webpack(config) {
+        config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "./"), // Add the alias here
+        };
+        return config;
     },
 };
 
