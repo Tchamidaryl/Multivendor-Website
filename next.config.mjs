098c7a417bdba8b19c7f652cb58301d40a63c317
Tchamidaryl <tchamidaryl@gmail.com>
2025-03-18 23:40:12 +0100

    test-8

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,3 @@
-import path from "path"; // Add this import at the top
-
-// Workaround to get __dirname in ES modules
-const __dirname = path.resolve();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -12,13 +7,6 @@ const nextConfig = {
             hostname: "utfs.io",
         },
         ],
-    },
-    webpack(config) {
-        config.resolve.alias = {
-        ...config.resolve.alias,
-        "@": path.resolve(__dirname, "./"), // Add the alias here
-        };
-        return config;
     },
 };
 
